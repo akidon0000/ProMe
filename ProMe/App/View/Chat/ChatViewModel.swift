acf40a64791acb8d86dcadef4f700a277a86e9ee
akidon0000 <a@art.jp>
2023-05-01 10:44:45 +0900

    [Add]chatViewModel追加

---
+++ ProMe/App/View/Chat/ChatViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ChatViewModel.swift
+//  ProMe
+//
+//  Created by Akihiro Matsuyama on 2023/05/01.
+//
+
+//WARNING// import UIKit 等UI関係は実装しない
+import Foundation
+
+class ChatViewModel {
+    
+}
