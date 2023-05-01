2aa2b66ec5d1c391500ffeed312058c2ca518549
Akihiro Matsuyama <a@art.jp>
2023-05-01 09:51:52 +0900

    init commit

---
+++ ProMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProMe
+//
+//  Created by Akihiro Matsuyama on 2023/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
