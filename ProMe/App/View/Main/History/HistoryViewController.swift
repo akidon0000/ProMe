40c954f1de65047e7189873f7e5b24318a1f52ac
akidon0000 <a@art.jp>
2023-05-07 14:54:48 +0900

    [Add]History追加

---
+++ ProMe/App/View/Main/History/HistoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HistoryViewController.swift
+//  ProMe
+//
+//  Created by Akihiro Matsuyama on 2023/05/07.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
